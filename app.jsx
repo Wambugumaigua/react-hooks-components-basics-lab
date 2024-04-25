eebcfaf1c1354e2de99e5839e00601e12c6f0827
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-04-24 23:03:23 -0700

    components

---
+++ app.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
